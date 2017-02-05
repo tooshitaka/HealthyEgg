60b3928f23a09a0be316e32b059c601a24ba7a2c
tooshitaka <cit015007@g.nihon-u.ac.jp>
2017-02-06 02:08:09 +0900

    Add graph label

--- Game Files/Assets/Scripts/Lifelog/Bar.cs
+++ Game Files/Assets/Scripts/Lifelog/Bar.cs
@@ -6,6 +6,9 @@ using System.Collections.Generic;
 public class Bar : MonoBehaviour {
     
     public Image bar;
+    public Text label;
+    public Text value;
+
 	// Use this for initialization
 	void Start () {
 	
