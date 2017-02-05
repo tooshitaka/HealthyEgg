2fc3cc7dd2fc6a9d6163341faa8e5b905c279888
tooshitaka <cit015007@g.nihon-u.ac.jp>
2017-02-05 15:06:05 +0900

    Attatch DropDownOptions script to dropdown in Preset

---
+++ Game Files/Assets/Scripts/Preset/DropDownOptions.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class DropDownOptions : MonoBehaviour {
+
+    public Dropdown dropdown;
+    public Text selectedName;
+
+    List<string> names = new List<string>() { "Fred", "Banny", "Wilma", "Betty" };
+
+    public void Dropdown_indexChanged(int index)
+    {
+        selectedName.text = names[index];
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        SetListOptions();
+    }
+
+    // Set preset names to the dropdown lists
+    void SetListOptions()
+    {
+        dropdown.AddOptions(names);
+    }
+}
