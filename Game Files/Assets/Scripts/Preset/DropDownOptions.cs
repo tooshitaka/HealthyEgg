5f570d8aa092059b68d58796015d0e2f6f8906a0
tooshitaka <cit015007@g.nihon-u.ac.jp>
2017-02-05 15:40:42 +0900

    Add the feature that calorie changes based on the selected preset

--- Game Files/Assets/Scripts/Preset/DropDownOptions.cs
+++ Game Files/Assets/Scripts/Preset/DropDownOptions.cs
@@ -6,18 +6,32 @@ using UnityEngine.UI;
 public class DropDownOptions : MonoBehaviour {
 
     public Dropdown dropdown;
-    public Text selectedName;
+    public Text calorie;
 
-    List<string> names = new List<string>() { "Fred", "Banny", "Wilma", "Betty" };
+    List<string> names = new List<string>() { "Select your preset" };
 
+    // Get preset names from database
+    void GetPresetFromDatabase()
+    {
+        // TODO: Get the current user's preset from database
+        names.Add("Breakfast");
+        names.Add("さんまラーメン");
+    }
+
+    // Eventhandler when one preset selected
     public void Dropdown_indexChanged(int index)
     {
-        selectedName.text = names[index];
+        // TODO: Fill the calorie of selected preset 
+        List<string> calorieList = new List<string>() { "0", "500", "800" }; 
+        calorie.text = calorieList[index];
     }
 
     // Use this for initialization
     void Start()
     {
+        // Get preset names from database
+        GetPresetFromDatabase();
+        // Set preset names to dropdown options
         SetListOptions();
     }
 
