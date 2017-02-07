f51481f319dfee3241fb4b68706781b9085e6ade
tooshitaka <cit015007@g.nihon-u.ac.jp>
2017-02-07 10:50:01 +0900

    Adjust the movement of the creature

--- Game Files/Assets/Scripts/Creature/CreatureBehavior.cs
+++ Game Files/Assets/Scripts/Creature/CreatureBehavior.cs
@@ -7,24 +7,39 @@ public class CreatureBehavior : MonoBehaviour
     private Vector3 velocity = Vector3.zero;
     public bool grounded = true;
 
-    public float speed = 1.5f;
+    public float speed = 0.25f;
+
+    public int timer = 0;
+    public int timer_interval = 25;
 
     void Start()
     {
-        velocity.x = 1f;
     }
 
     void Update()
     {
-        Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
-        pos.x = Mathf.Clamp01(pos.x);
-        pos.y = Mathf.Clamp01(pos.y);
-        transform.position = Camera.main.ViewportToWorldPoint(pos);
+        // Move creature left to right
+        if (timer < timer_interval)
+        {
+            transform.Translate(speed, 0, 0);
+        }else
+        {
+            transform.Translate(-speed, 0, 0);
+        }
+        timer++;
+        // Reset timer
+        if (timer > 50) 
+            timer = 0;
 
-        if (pos.x == 1 || pos.x == 0)
-            velocity *= -1;
+        //Vector3 pos = Camera.main.WorldToViewportPoint(transform.position);
+        //pos.x = Mathf.Clamp01(pos.x);
+        //pos.y = Mathf.Clamp01(pos.y);
+        //transform.position = Camera.main.ViewportToWorldPoint(pos);
 
-        transform.Translate(velocity * speed);
+        //if (pos.x == 1 || pos.x == 0)
+        //    velocity *= -1;
+
+        //transform.Translate(velocity * speed);
     }
 
 
